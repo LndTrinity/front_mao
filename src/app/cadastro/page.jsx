6afdc7fd72b5cc9022ec31d0b2197ba50462c79a
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2024-11-10 23:55:38 -0300

    add axios for API requests and implement login functionality

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,73 +1,111 @@
+"use client";
+import {useState} from "react";
+import axios from "axios";
 
-export default function cadastro() {
+export default function Cadastro() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [NomeCompleto, setNomeCompleto] = useState("");
+  const [tipo, setTipo] = useState("CLIENTE");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (senha !== confirmarSenha) {
+      alert("As senhas não coincidem.");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_URL_API}/usuarios/signin`,
+        {
+          nome: NomeCompleto,
+          email,
+          senha,
+          tipo,
+        }
+      );
+      console.log("Usuário registrado:", response.data);
+    } catch (error) {
+      console.error("Erro ao registrar usuário:", error);
+    }
+  };
+
   return (
-    
-  <main className="bg-[url('../../public/imagem_profissional.png')]  bg-no-repeat bg-right-bottom   ">
-    
-  
-    <div className="flex justify-center items-center min-h-screen p-4">
-     
-      <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
-      
-        <h2 className="text-gray-200 text-center text-2xl font-bold mb-6">
-          Cadastro
-        </h2>
-        <form>
-          <input
-            type="email"
-            placeholder="Seu melhor e-mail"
-            className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Confirmar senha"
-            className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
-            required
-          />
-          <input
-            type="text"
-            placeholder="Primeiro nome"
-            className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
-            required
-          />
-          <input
-            type="text"
-            placeholder="Sobrenome"
-            className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
-            required
-          />
-          <input
-            type="tel"
-            placeholder="Celular (53) 98112-3456"
-            className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
-            required
-          />
-          <button
-            type="submit"
-            className="w-full mt-6 bg-yellow-300 text-dark p-2 rounded hover:bg-yellow-400 transition"
-          >
-            Registrar
-          </button>
-        </form>
-        <div className="text-center mt-4">
-          <p className="text-gray-200">
-            Já tem uma conta?
-            <a
-              href="index.html"
-              className="text-yellow-600 text-sm hover:text-yellow-500"
-              >Login </a>
-          </p>
+    <main className="bg-[url('../../public/imagem_profissional.png')] bg-no-repeat bg-right-bottom">
+      <div className="flex justify-center items-center min-h-screen p-4">
+        <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
+          <h2 className="text-gray-200 text-center text-2xl font-bold mb-6">
+            Cadastro
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Nome completo"
+              className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+              required
+              value={NomeCompleto}
+              onChange={(e) => setNomeCompleto(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Seu melhor e-mail"
+              className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+              required
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirmar senha"
+              className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+              required
+              value={confirmarSenha}
+              onChange={(e) => setConfirmarSenha(e.target.value)}
+            />
+
+            <div className="mb-4">
+              <label className="text-gray-200">Tipo de usuário:</label>
+              <div className="flex mt-2">
+                <label className="text-gray-200 mr-4">
+                  <input
+                    type="radio"
+                    value="CLIENTE"
+                    checked={tipo === "CLIENTE"}
+                    onChange={(e) => setTipo(e.target.value)}
+                    className="mr-2"
+                  />
+                  Cliente
+                </label>
+                <label className="text-gray-200">
+                  <input
+                    type="radio"
+                    value="PRESTADOR"
+                    checked={tipo === "PRESTADOR"}
+                    onChange={(e) => setTipo(e.target.value)}
+                    className="mr-2"
+                  />
+                  Prestador
+                </label>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-teal-500 text-white p-2 rounded hover:bg-teal-600 transition-colors"
+            >
+              Registrar
+            </button>
+          </form>
         </div>
       </div>
-    </div>
-    
-  </main>
-
-  )}
+    </main>
+  );
+}
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,36 +1,60 @@
-export default function login() {
+"use client";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_URL_API}/usuarios/login`,
+        {
+          email,
+          senha,
+        }
+      );
+      console.log("Usuário logado:", response.data);
+    } catch (error) {
+      console.error("Erro ao logar:", error);
+    }
+  };
+
   return (
-    <main className="bg-[url('../../public/imagem_profissional.png')]  bg-no-repeat bg-right-bottom   ">
-
-
+    <main className="bg-[url('../../public/imagem_profissional.png')] bg-no-repeat bg-right-bottom">
       <div className="background-image"></div>
 
       <div className="flex justify-center items-center min-h-screen p-4">
-
         <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
-
           <h2 className="text-gray-200 text-center text-2xl font-bold mb-6">
             Mão na Roda
           </h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="email"
               placeholder="E-mail"
               className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
               placeholder="Senha"
               className="w-full mb-4 p-2 rounded bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
               required
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
             />
             <div className="flex justify-between items-center mb-4">
-              <label className="text-gray-200 flex items-center">
-                <input type="checkbox" className="mr-2" /> Lembrar-me por 14 dias
-              </label>
-              <a href="#" className="text-yellow-600 text-sm hover:text-yellow-500"
-              >Esqueceu a senha?</a            >
+              <a
+                href="#"
+                className="text-yellow-600 text-sm hover:text-yellow-500"
+              >
+                Esqueceu a senha?
+              </a>
             </div>
             <button
               type="submit"
@@ -45,12 +69,13 @@ export default function login() {
               <a
                 href="cadastro.html"
                 className="text-yellow-600 text-sm hover:text-yellow-500"
-              >Cadastre-se aqui</a            >
+              >
+                Cadastre-se aqui
+              </a>
             </p>
           </div>
         </div>
       </div>
-
     </main>
-  )
+  );
 }
